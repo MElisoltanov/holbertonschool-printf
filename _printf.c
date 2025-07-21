2f642b5bf48799fc5a0956b328a80308514e7f3e
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-21 13:57:41 +0200

    Add function skeletons to all printf source files

---
+++ _printf.c
@@ -0,0 +1,15 @@
+/**
+ * _printf - Produces output according to a format
+ * @format: A string with format specifiers
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    // Étape 1 : Initialiser les variables nécessaires (compteur, va_list)
+    // Étape 2 : Parcourir la chaîne format caractère par caractère
+    // Étape 3 : Si le caractère est %, vérifier le caractère suivant
+    // Étape 4 : Selon le spécificateur, appeler la bonne fonction
+    // Étape 5 : Gérer le cas d'un pourcentage littéral (%%)
+    // Étape 6 : Imprimer les caractères standards avec _putchar
+    // Étape 7 : Fermer va_list et retourner le nombre de caractères imprimés
+}
