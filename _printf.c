f2cd4af28b3277ae00126574dc00daeeb861fdcb
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-22 16:08:22 +0200

    Update _printf to delegate specifier handling to function pointers

--- _printf.c
+++ _printf.c
@@ -8,11 +8,9 @@
 
 int _printf(const char *format, ...)
 {
-	/* Étape 1 : Initialiser les variables nécessaires (compteur, va_list) */
 	va_list args;
 	int count = 0;
 	int i;
-	/* int j; */
 	int (*func_ptr)(va_list);
 
 	va_start(args, format);
@@ -22,10 +20,8 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
 
-	/* Étape 2 : Parcourir la chaîne format caractère par caractère */
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		/* Étape 3 : Si le caractère est %, vérifier le caractère suivant */
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
@@ -34,49 +30,22 @@ int _printf(const char *format, ...)
 				return (-1);
 			}
 
-			/* Étape 4 : Selon le spécificateur, appeler la bonne fonction */
 			if (format[i + 1] == 'c')
 			{
-				/*
-				_putchar(va_arg(args, int));
-				count++;
-				i++;
-				continue;
-				*/
-
 				func_ptr = print_char;
 				count += func_ptr(args);
 				i++;
 				continue;
 			}
 
-			/* Étape 4 : Selon le spécificateur, appeler la bonne fonction */
 			else if (format[i + 1] == 's')
 			{
-				
-				/*
-				char *s = va_arg(args, char *);
-				if (s == NULL)
-				{
-					s = "(null)";
-				}
-
-				for (j = 0; s[j] != '\0'; j++)
-				{
-					_putchar(s[j]);
-					count++;
-				}
-				i++;
-				continue;
-				*/
-
 				func_ptr = print_string;
 				count += func_ptr(args);
 				i++;
 				continue;
 			}
 
-			/* Étape 4 : Selon le spécificateur, appeler la bonne fonction */
 			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
 			{
 				func_ptr = print_number;
@@ -85,7 +54,6 @@ int _printf(const char *format, ...)
 				continue;
 			}
 
-			/* Étape 5 : Gérer le cas d'un pourcentage littéral (%%) */
 			else if (format[i + 1] == '%')
 			{
 				_putchar('%');
@@ -98,7 +66,6 @@ int _printf(const char *format, ...)
 			count++;
 		}
 
-		/* Étape 6 : Imprimer les caractères standards avec _putchar */
 		else 
 		{
 			_putchar(format[i]);
@@ -106,7 +73,6 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	/* Étape 7 : Fermer va_list et retourner le nombre de caractères imprimés */
 	va_end(args);
 	return (count);
 }
