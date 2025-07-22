fe6885d6dcfb086f1a5201c11a6e3b7105cc95d9
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-22 16:00:17 +0200

    Implement print_char function to handle %c format specifier

--- write_char.c
+++ write_char.c
@@ -1,11 +1,17 @@
+#include "main.h"
+
 /**
  * print_char - Prints a single character
  * @args: Argument list containing the character to print
  * Return: Number of characters printed (always 1)
  */
+
 int print_char(va_list args)
 {
-    // Étape 1 : Récupérer le caractère depuis la liste d’arguments
-    // Étape 2 : Utiliser _putchar pour l’afficher
-    // Étape 3 : Retourner 1 (un seul caractère affiché)
+    /* Étape 1 : Récupérer le caractère depuis la liste d’arguments */
+    /* Étape 2 : Utiliser _putchar pour l’afficher */
+    _putchar(va_arg(args, int));
+
+    /* Étape 3 : Retourner 1 (un seul caractère affiché) */
+    return (1);
 }
