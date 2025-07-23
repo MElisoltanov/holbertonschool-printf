2dc224737492bc2c8d6958b712f6bf679b236725
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-23 13:32:46 +0200

    Refactor _printf function and implement handle_specifier helper

--- _printf.c
+++ _printf.c
@@ -1,6 +1,52 @@
 #include "main.h"
 
 /**
+ * handle_specifier - Handles format specifiers
+ * and calls corresponding function
+ * @format: The format string
+ * @args: va_list of arguments
+ * @i: Pointer to the current index in format string
+ *
+ * Return: Number of characters printed for the current specifier
+ */
+
+int handle_specifier(const char *format, va_list args, int *i)
+{
+	int (*func_ptr)(va_list);
+
+	if (format[*i + 1] == 'c')
+	{
+		func_ptr = print_char;
+	}
+
+	else if (format[*i + 1] == 's')
+	{
+		func_ptr = print_string;
+	}
+
+	else if (format[*i + 1] == 'd' || format[*i + 1] == 'i')
+	{
+		func_ptr = print_number;
+	}
+
+	else if (format[*i + 1] == '%')
+	{
+		_putchar('%');
+		(*i)++;
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	(*i)++;
+	return (func_ptr(args));
+}
+
+
+/**
  * _printf - Produces output according to a format
  * @format: A string with format specifiers
  * Return: Number of characters printed
@@ -9,9 +55,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	int i;
-	int (*func_ptr)(va_list);
+	int count = 0, i;
 
 	va_start(args, format);
 
@@ -30,47 +74,12 @@ int _printf(const char *format, ...)
 				return (-1);
 			}
 
-			if (format[i + 1] == 'c')
-			{
-				func_ptr = print_char;
-				count += func_ptr(args);
-				i++;
-				continue;
-			}
-
-			else if (format[i + 1] == 's')
-			{
-				func_ptr = print_string;
-				count += func_ptr(args);
-				i++;
-				continue;
-			}
-
-			else if (format[i + 1] == 'd' || format[i + 1] == 'i')
-			{
-				func_ptr = print_number;
-				count += func_ptr(args);
-				i++;
-				continue;
-			}
-
-			else if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				count++;
-				i++;
-				continue;
-			}
-
-			_putchar(format[i]);
-			count++;
+			count += handle_specifier(format, args, &i);
+			continue;
 		}
 
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
+		_putchar(format[i]);
+		count++;
 	}
 
 	va_end(args);
